a3ca2bc9c96ebdd5fde25feaca6e7fb9d30ddd9c
danilojduarte <76825919+danilojduarte@users.noreply.github.com>
2024-07-05 13:55:27 -0300

    Criando components Navbar

--- primeiro-projeto/src/App.jsx
+++ primeiro-projeto/src/App.jsx
@@ -1,27 +1,15 @@
 import React from "react";
-import logoImg from './assets/img/logo-removebg-preview.png'
+import Navbar from "./components/Navbar/Navbar.jsx";
 
-import './styles/App.css'
+import './styles/App.css';
 
 class App extends React.Component {
   // Metodo responsável por renderizar o conteúdo HTML do nosso componente;
   render () {
-      return (
-       <header>
-        <nav id="navbar">
-          <div className="nav-brand">
-            <img src={logoImg} alt="Logo do planeta" />
-            <h1>Space Flight News</h1>
-          </div>
-          <ul className="nav-list">
-            <li><a href="/">Home</a></li>
-            <li><a href="/">Trending</a></li>
-            <li><a href="/">Categories</a></li>
-            <li><a href="/">About us</a></li>
-          </ul>
-        </nav>
-       </header>
-      );
+      return 
+      <Navbar />;
+
+      
     }
   }
   
