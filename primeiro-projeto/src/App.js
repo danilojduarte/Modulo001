7fb64e565b82c1781002eca86585fd2ab002820f
danilojduarte <76825919+danilojduarte@users.noreply.github.com>
2024-07-04 23:15:12 -0300

    Iniciando criação de aplicação com React

--- primeiro-projeto/src/App.js
+++ primeiro-projeto/src/App.js
@@ -2,17 +2,12 @@ import React from "react";
 
 class App extends React.Component {
   // Metodo responsável por renderizar o conteúdo HTML do nosso componente;
-  constructor() {
-    super();
-    this.nome = "Danilo Duarte";
-  }
-
   render () {
       return (
         <div>
           <h1>Primeiro Projeto</h1>
     
-          <h2>{this.nome}</h2>
+          
 
           <p>Realizando teste</p>
         </div>
