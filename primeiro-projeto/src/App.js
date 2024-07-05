adee9b2bee986947ed7c5183119f2b841977aaa5
danilojduarte <76825919+danilojduarte@users.noreply.github.com>
2024-07-04 23:11:45 -0300

    Criando primeiro Construtor()

--- primeiro-projeto/src/App.js
+++ primeiro-projeto/src/App.js
@@ -2,11 +2,18 @@ import React from "react";
 
 class App extends React.Component {
   // Metodo responsável por renderizar o conteúdo HTML do nosso componente;
+  constructor() {
+    super();
+    this.nome = "Danilo Duarte";
+  }
+
   render () {
       return (
         <div>
           <h1>Primeiro Projeto</h1>
     
+          <h2>{this.nome}</h2>
+
           <p>Realizando teste</p>
         </div>
       );
