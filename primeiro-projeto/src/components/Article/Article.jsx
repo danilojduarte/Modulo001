ee25dcf076ec3ad43888136a4e6fec8340d4471d
danilojduarte <76825919+danilojduarte@users.noreply.github.com>
2024-07-08 02:41:18 -0300

    Primeiro artigo concluido, gerando novos artigos

--- primeiro-projeto/src/components/Article/Article.jsx
+++ primeiro-projeto/src/components/Article/Article.jsx
@@ -14,7 +14,7 @@ export class Article extends React.Component {
         <div className="article-infos">
           <h2>House Of Dragon</h2>
           <h3>TechInf</h3>
-          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero unde, neque natus nemo quos ipsa rerum molestias esse quod. Doloremque dicta, quo officiis sint delectus tempore excepturi accusantium! Minima, nobis.</p>
+          <p>Os apoiadores de Aegon Targaryen entram em conflito com os adeptos de sua meia-irmã, Rhaenyra, pelo trono de Viserys I, seu falecido pai, iniciando uma guerra civil cerca de 200 anos antes dos eventos retratados em "Game of Thrones".</p>
         </div>
       </article>
     )
